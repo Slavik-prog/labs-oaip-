aa0b387d584170d7ec5f111cab9d33c1838b3acd
Slavik-prog <slawalol81@gmail.com>
2025-01-14 13:38:02 +0300

    Add files via upload

---
+++ laba 2/laba 2/dop5.2/dop5.2/dop5.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main() {
+    // Начало
+    double x, y, z;
+
+    // Ввод x, y, z
+    std::cout << "Введите x, y, z: ";
+    std::cin >> x >> y >> z;
+
+    // Вычисление суммы и произведения
+    double sum = x + y + z;
+    double product = x * y * z;
+
+    // Определение максимального значения
+    double maxValue;
+    if (sum >= product) {
+        maxValue = sum;
+    }
+    else {
+        maxValue = product;
+    }
+
+    // Результат
+    double result = maxValue * 3;
+
+    // Вывод результата
+    std::cout << "Результат: " << result << std::endl;
+
+    // Конец
+    return 0;
+}
